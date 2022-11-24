621721fd4038b6101ce60cd5588f8bb5f4104990
Siva <sivasubrahmanyam.pagolu@techwave.net>
2022-11-24 22:15:42 +0530

    service impl changes

--- src/main/java/com/ensat/services/ProductServiceImpl.java
+++ src/main/java/com/ensat/services/ProductServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Product service implement.
+ * Product service implementation.
  */
 @Service
 public class ProductServiceImpl implements ProductService {
