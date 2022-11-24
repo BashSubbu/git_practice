f158d7b7be5955e0704a1505f167dc86eeb27d55
Siva <sivasubrahmanyam.pagolu@techwave.net>
2022-11-24 21:45:43 +0530

    added comment in repo

--- src/main/java/com/ensat/repositories/ProductRepository.java
+++ src/main/java/com/ensat/repositories/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.ensat.repositories;
 
 import com.ensat.entities.Product;
+
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    // for getting product information
     Product geProduct(int id);
 
 }
